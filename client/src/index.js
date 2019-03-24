ce2457eb38f39cc1cdbc601e7d7b8622d7ee9e19
ianbunn <ianrichardbunn@gmail.com>
2019-03-24 12:23:58 -0500

    fixing config keys

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import dotenv from "dotenv"
-dotenv.config("../.env")
+// import dotenv from "dotenv"
+// dotenv.config("../.env")
+
+import { } from "dotenv/config"
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
