f84daabc8570092f4e62428a575a4247fd4115d8
ianbunn <ianrichardbunn@gmail.com>
2019-03-24 08:59:52 -0500

    switched from config_keys.js to .env

--- client/src/utils/Api.js
+++ client/src/utils/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
-import GOOGLE_BOOKS_API from "../config_keys"
+// import GOOGLE_BOOKS_API from "../config_keys"
 
-const APIKEY = GOOGLE_BOOKS_API;
+const APIKEY = process.env.GOOGLE_BOOKS_API;
 const BASEURL = `https://www.googleapis.com/books/v1/volumes?key=${APIKEY}&orderBy=relevance`
 
 export default {
